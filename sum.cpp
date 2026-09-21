4603ad668479d25e8b145dceb7d4e7681419e74d
dkim144277 <dkim144277@me.bergen.edu>
2026-09-21 14:07:22 -0400

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int num1;
+    int num2;
+    int total;
+    num1 = 50;
+    num2 = 100;
+    
+    total = num1 + num2;
+    
+    std::cout << "Total of 50 + 100 = " << total ;
+    return 0;
+}
